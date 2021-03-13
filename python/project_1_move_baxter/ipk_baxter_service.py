dfd6c2db8e126d15de46b36f0c738cb193392d71
Elkinmt19 <elkin02011@hotmail.com>
2021-03-12 20:53:11 -0500

    ipk_baxter_service.py script added

---
+++ python/project_1_move_baxter/ipk_baxter_service.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+"""
+Baxter RSDK Inverse Kinematics 
+"""
+import struct
+import numpy as np
+
+import rospy
+import baxter_interface
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+
+def ikp_baxter(limb):
+    rospy.init_node("ipk_baxter_service_client")
+    ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    ikreq = SolvePositionIKRequest()
+    hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+    poses = {
+        'left': PoseStamped(
+            header=hdr,
+            pose=Pose(
+                position=Point(
+                    x=0.657579481614,
+                    y=0.851981417433,
+                    z=0.0388352386502,
+                ),
+                orientation=Quaternion(
+                    x=-0.366894936773,
+                    y=0.885980397775,
+                    z=0.108155782462,
+                    w=0.262162481772,
+                ),
+            ),
+        ),
+        'right': PoseStamped(
+            header=hdr,
+            pose=Pose(
+                position=Point(
+                    x=0.656982770038,
+                    y=-0.852598021641,
+                    z=0.0388609422173,
+                ),
+                orientation=Quaternion(
+                    x=0.367048116303,
+                    y=0.885911751787,
+                    z=-0.108908281936,
+                    w=0.261868353356,
+                ),
+            ),
+        ),
+    }
+
+    ikreq.pose_stamp.append(poses[limb])
+    try:
+        rospy.wait_for_service(ns, 5.0)
+        resp = iksvc(ikreq)
+    except (rospy.ServiceException, rospy.ROSException), e:
+        rospy.logerr("Service call failed: %s" % (e,))
+        return 1
+
+    # Check if result valid, and type of seed ultimately used to get solution
+    # convert rospy's string representation of uint8[]'s to int's
+    resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
+                               resp.result_type)
+    if (resp_seeds[0] != resp.RESULT_INVALID):
+        seed_str = {
+                    ikreq.SEED_USER: 'User Provided Seed',
+                    ikreq.SEED_CURRENT: 'Current Joint Angles',
+                    ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+                   }.get(resp_seeds[0], 'None')
+        print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
+              (seed_str,))
+        # Format solution into Limb API-compatible dictionary
+        limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+        print "\nIK Joint Solution:\n", limb_joints
+        print "------------------"
+        print "Response Message:\n", resp
+    else:
+        print("INVALID POSE - No Valid Joint Solution Found.")
+
+    return limb_joints
+
+def set_position(cmd,arm):
+    right_arm = baxter_interface.Limb('right')
+    left_arm = baxter_interface.Limb('left')
+
+    if arm == 'right':
+        joint_command = cmd
+        print(joint_command)
+        right_arm.move_to_joint_positions(joint_command)
+
+    if arm == 'left':
+        joint_command = cmd
+        print(joint_command)
+        left_arm.move_to_joint_positions(joint_command)
+
+
+def main():
+    joint_cmd = ikp_baxter('right')
+    set_position(joint_cmd,'right')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+		print 'BAXTER ERROR'
