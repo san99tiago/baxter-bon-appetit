a5fe1264737c836991bec1f884005392ee9b36fb
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-04-10 18:13:54 -0500

    Updated baxter_essentials transformation to add get_fixed_angles_from_tm

--- python/baxter_bon_appetit/src/baxter_essentials/transformation.py
+++ python/baxter_bon_appetit/src/baxter_essentials/transformation.py
@@ -9,7 +9,7 @@ import numpy as np
 
 class Transformation:
     """
-    Get transformation matrix from fixed-angles rotations and a translation.
+    Transformation matrix operations.
 
     :param angle_x: rotation of {B} with respect of {A} in "x" axis (radians).
     :param angle_y: rotation of {B} with respect of {A} in "y" axis (radians).
@@ -34,7 +34,7 @@ class Transformation:
         self.create_rotation_matrix("z", angle_z)
 
         # Create homogeneous transformation matrix
-        self.create_transformation_matriz()
+        self.create_transformation_matrix()
 
     def create_rotation_matrix(self, axis, angle):
         """
@@ -65,14 +65,45 @@ class Transformation:
                 [math.sin(angle), math.cos(angle), 0],
                 [0, 0, 1]
             ])
-    
-    def create_transformation_matriz(self):
+
+    def create_transformation_matrix(self):
+        """
+            Get transformation matrix from fixed-angles rotations and a 
+            translation.
+        """
         # Get rotation matrix with fixed angles approach (Rxyz = Rz.Ry.Rx)
         R_ZYX = np.dot(np.dot(self.RZ, self.RY), self.RX)
 
         extra_zeros_perspective = np.array([[0, 0, 0]])
         self.TM = np.concatenate((R_ZYX, extra_zeros_perspective))
         self.TM = np.concatenate((self.TM, self.vector_AB), axis=1)
+
+    def get_fixed_angles_from_tm(self, tm):
+        """
+        Get fixed angles from transformation matrix.
+            Remark: these equations are from Craig Robotics book
+        :param tm: Tranformation Matrix.
+        :returns: array with angles in radians with the following structure.
+            np.array([angle_x, angle_y, angle_z]), where "x_angle", "y_angle", 
+            and "z_angle" are the specific rotations given the axis.
+        """
+
+        y_angle = math.atan2(-tm[2, 0], math.sqrt(tm[0, 0]**2 + tm[1, 0]**2))
+
+        # Compute boundaries cases if necessary (-pi/2 and pi/2)
+        if (y_angle == math.pi/2):
+            z_angle = 0
+            x_angle = math.atan2(tm[0, 1], tm[1, 1])
+        elif (y_angle == -math.pi/2):
+            z_angle = 0
+            x_angle = -math.atan2(tm[0, 1], tm[1, 1])
+        else:
+            z_angle = math.atan2(
+                tm[1, 0]/math.cos(y_angle), tm[0, 0]/math.cos(y_angle))
+            x_angle = math.atan2(
+                tm[2, 1]/math.cos(y_angle), tm[2, 2]/math.cos(y_angle))
+
+        return([x_angle, y_angle, z_angle])
 
 
 # TESTS
@@ -86,6 +117,7 @@ if __name__ == "__main__":
     print(test.RZ)
     print("TM:")
     print(test.TM)
+    test.get_fixed_angles_from_tm(test.TM)
 
     test = Transformation(0, 0, math.radians(90), [1, 2, 3])
     print("RX:")
@@ -96,3 +128,16 @@ if __name__ == "__main__":
     print(test.RZ)
     print("TM:")
     print(test.TM)
+    print(test.get_fixed_angles_from_tm(test.TM))
+
+    test = Transformation(math.radians(30), math.radians(
+        55), math.radians(90), [1, 2, 3])
+    print("RX:")
+    print(test.RX)
+    print("RY:")
+    print(test.RY)
+    print("RZ:")
+    print(test.RZ)
+    print("TM:")
+    print(test.TM)
+    print(test.get_fixed_angles_from_tm(test.TM))
