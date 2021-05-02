a7ee9c9571d4bbeb2b074b8df06ed1a9cf60f6b1
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-05-02 18:43:56 -0500

    Update baxter_control with constraints for MPC and correct angles_increment

--- python/baxter_bon_appetit/src/baxter_control/mpc_controller.py
+++ python/baxter_bon_appetit/src/baxter_control/mpc_controller.py
@@ -21,11 +21,44 @@ class MpcController:
     :param N: Prediction horizon (must be a positive integer).
     """
 
-    def __init__(self, N, show_results):
+    def __init__(self, N, has_max_min_constraints, show_results):
         self.N = N  # Prediction horizon
         self.nu = 7  # Number of inputs (thetas)
         self.nx = 7  # Number of states (delta of thetas)
         self.show_results = show_results
+        self.has_max_min_constraints = has_max_min_constraints
+
+        self.define_min_max_thetas()
+
+    def define_min_max_thetas(self):
+        """
+        Defines the vectors for the maximum and minimum values for the thetas 
+        angles of Baxter's arms in radians.
+        """
+        # Values obtained from article "Baxter Humanoid Robot Kinematics"
+        self.min_thetas = np.array(
+            [
+                math.radians(-141),
+                math.radians(-123),
+                math.radians(-173),
+                math.radians(-3),
+                math.radians(-175),
+                math.radians(-90),
+                math.radians(-175)
+            ]
+        ).reshape((7, ))
+
+        self.max_thetas = np.array(
+            [
+                math.radians(51),
+                math.radians(60),
+                math.radians(173),
+                math.radians(150),
+                math.radians(175),
+                math.radians(120),
+                math.radians(175)
+            ]
+        ).reshape((7, ))
 
     def execute_mpc(self, cartesian_goal, current_thetas):
         """
@@ -36,6 +69,9 @@ class MpcController:
             Example: np.array([x, y, z, angle_x, angle_y, angle_z]).reshape(6, 1)
         :param current_thetas: Current state of the robot's degrees of freedom.
             Example: np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]).reshape(7, 1)
+        :returns: Dictionary with the structure of 
+            {"optimal_thetas": np.array, "optimal_dthetas": np.array}. If there 
+            are NO optimal answers, it returns "None".
         """
         # Define X and U vectors (thetas and delta-thetas for the MPC)
         if (sys.version_info[0] >= 3):
@@ -48,6 +84,7 @@ class MpcController:
         # Initialize cost function and constraints for the MPC problem
         cost = 0.0
         constr = []
+
         for t in range(self.N):
             # Calculate variables for the construction of the cost function
             cartesian_increment = self.calculate_cartesian_increment(
@@ -59,10 +96,10 @@ class MpcController:
             print("cartesian_increment.shape:")
             print(cartesian_increment.shape)
             print("dthetas[:,t]:")
-            print(dthetas[:,t])
+            print(dthetas[:, t])
             print("jacobian * dthetas[:, t]).shape:")
             #print((jacobian * dtheas[:, t]).shape)
-            inner_error = cartesian_increment - jacobian * dthetas[:,t]
+            inner_error = cartesian_increment - jacobian * dthetas[:, t]
             cost += cvxpy.quad_form(inner_error, np.eye(6))
 
             # Constaint for: Thetas[k+1] = Thetas[k] + DeltaThetas[k]
@@ -71,6 +108,13 @@ class MpcController:
 
             if (self.show_results):
                 print("cost[{}] = {}".format(t, cost))
+
+            if (self.has_max_min_constraints):
+                # Constraint for min values of thetas
+                constr += [thetas[:, t] >= self.min_thetas]
+
+                # Constraint for max values of thetas
+                constr += [thetas[:, t] <= self.max_thetas]
 
         # Constraint for initial state: Theta[k] = ThetaCurrent
         constr += [thetas[:, 0] == current_thetas[:, 0]]
@@ -90,6 +134,8 @@ class MpcController:
             self.optimal_thetas = thetas.value
             self.optimal_dthetas = dthetas.value
             return {"optimal_thetas": self.optimal_thetas, "optimal_dthetas": self.optimal_dthetas}
+        else:
+            return None
 
     def calculate_cartesian_increment(self, cartesian_goal, current_thetas):
         """
@@ -110,8 +156,9 @@ class MpcController:
         # Get [x_angle, y_angle, z_angle] from current transformation matrix
         t = transf.Transformation(0, 0, 0, [0, 0, 0])  # Fake init (see below)
         fixed_angles_from_tm = t.get_fixed_angles_from_tm(current_tm)
-        current_orientation_angles = np.array(fixed_angles_from_tm).reshape(3, 1)
-        
+        current_orientation_angles = np.array(
+            fixed_angles_from_tm).reshape(3, 1)
+
         # Concatenate [x, y, z]' and [x_angle, y_angle, z_angle]'
         cartesian_current = np.concatenate(
             [current_position, current_orientation_angles], axis=0)
@@ -156,14 +203,13 @@ class MpcController:
 
 
 if __name__ == "__main__":
-    MPC = MpcController(1, True)
+    MPC = MpcController(1, True, True)
     print()
 
     cartesian_goal = np.array([-0.5, -0.8, 1.0, 0, 0, 0]).reshape(6, 1)
     current_thetas = np.array(
         [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]).reshape(7, 1)
     mpc_results = MPC.execute_mpc(cartesian_goal, current_thetas)
-
 
     print('mpc_results["optimal_thetas"]:')
     print(mpc_results["optimal_thetas"])
@@ -179,18 +225,22 @@ if __name__ == "__main__":
     print("...fpk[current_thetas]...")
     tm_current = bc.BaxterClass().fpk(current_thetas, "right", 7)
     current_position = tm_current[0:3, 3]
-    current_orientation = transf.Transformation(0, 0, 0, [0, 0, 0]).get_fixed_angles_from_tm(tm_current)
+    current_orientation = transf.Transformation(
+        0, 0, 0, [0, 0, 0]).get_fixed_angles_from_tm(tm_current)
 
     # NEW CARTESIAN POSITION CALCULATIONS...
     print("...fpk[new_thetas]...")
     tm_new = bc.BaxterClass().fpk(new_thetas, "right", 7)
 
     new_position = tm_new[0:3, 3]
-    new_orientation = transf.Transformation(0, 0, 0, [0, 0, 0]).get_fixed_angles_from_tm(tm_new)
+    new_orientation = transf.Transformation(
+        0, 0, 0, [0, 0, 0]).get_fixed_angles_from_tm(tm_new)
 
     print("----CURRENT CARTESIAN----")
-    print(np.concatenate([current_position, current_orientation], axis=0).reshape(6, 1))
+    print(np.concatenate(
+        [current_position, current_orientation], axis=0).reshape(6, 1))
     print("----NEW CARTESIAN----")
-    print(np.concatenate([new_position, new_orientation], axis=0).reshape(6, 1))
+    print(np.concatenate(
+        [new_position, new_orientation], axis=0).reshape(6, 1))
     print("----CARTESIAN GOAL----")
     print(cartesian_goal)
