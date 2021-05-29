1f7867f006d422ed4edd17634021bc376e900a77
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-05-29 16:33:28 -0500

    Update computer vision unit-tests for face_detect <hr and fr>

--- python/computer_vision/face_detect_face_recognition/tests/test_process_image_fr.py
+++ python/computer_vision/face_detect_face_recognition/tests/test_process_image_fr.py
@@ -9,7 +9,7 @@ import cv2 as cv
 import numpy as np
 
 # My own imports
-import face_detect_hc as fdfr
+import face_detect_fr as fdfr
 import get_assets_folder as gaf
 
 # Get assets folder in repo for the samples
@@ -19,7 +19,7 @@ ASSETS_FOLDER = gaf.get_assets_folder_path()
 class TestProcessImage:
     def test_process_image_without_faces(self):
         """
-        Test that an real opened image without faces doen't return faces
+        Test that a real opened image without faces doesn't return faces
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "nofaces_0.jpg"))
@@ -27,11 +27,11 @@ class TestProcessImage:
         fd = fdfr.FaceDetector(
             real_image, show_results=False, only_biggest_face=True)
         faces = fd.face_detect()
-        assert (type(faces) is tuple)
+        assert (len(faces) == 0)
 
     def test_process_image_with_one_face(self):
         """
-        Test that an real opened image with one face, returns one face
+        Test that a real opened image with one face, returns one face
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "faces_0.jpg"))
@@ -46,7 +46,7 @@ class TestProcessImage:
 
     def test_process_image_with_two_faces(self):
         """
-        Test that an real opened image with two face, returns two faces
+        Test that a real opened image with two face, returns two faces
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "faces_1.jpg"))
--- python/computer_vision/face_detect_haar_cascade/tests/test_process_image_hc.py
+++ python/computer_vision/face_detect_haar_cascade/tests/test_process_image_hc.py
@@ -19,42 +19,42 @@ ASSETS_FOLDER = gaf.get_assets_folder_path()
 class TestProcessImage:
     def test_process_image_without_faces(self):
         """
-        Test that an real opened image without faces doen't return faces
+        Test that a real opened image without faces doesn't return faces
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "nofaces_0.jpg"))
         real_image = cv.imread(im_path)
         fd = fdhc.FaceDetector(real_image, show_results=False, only_biggest_face=True)
         faces = fd.face_detect()
-        assert (type(faces) is tuple)
+        assert (faces["detected_face"] == False)
 
     def test_process_image_with_one_face(self):
         """
-        Test that an real opened image with one face, returns one face
+        Test that a real opened image with one face, returns one face
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "faces_0.jpg"))
         real_image = cv.imread(im_path)
         fd = fdhc.FaceDetector(real_image, show_results=False, only_biggest_face=True)
         faces = fd.face_detect()
-        if (type(faces) is tuple):
+        if (faces["detected_face"] == False):
             AssertionError
 
-        assert len(faces) == 1
+        assert len(faces["faces"]) == 1
 
     def test_process_image_with_two_faces(self):
         """
-        Test that an real opened image with two face, returns two faces
+        Test that a real opened image with two face, returns two faces
         """
         im_path = os.path.abspath(os.path.join(
             ASSETS_FOLDER, "imgs", "faces_1.jpg"))
         real_image = cv.imread(im_path)
         fd = fdhc.FaceDetector(real_image, show_results=False, only_biggest_face=False)
         faces = fd.face_detect()
-        if (type(faces) is tuple):
+        if (faces["detected_face"] == False):
             AssertionError
 
-        assert len(faces) == 2
+        assert len(faces["faces"]) == 2
 
 
 if __name__ == '__main__':
